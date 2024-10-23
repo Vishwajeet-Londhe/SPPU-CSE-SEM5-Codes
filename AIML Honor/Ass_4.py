40bc922f932633cc9b0f6d2808b3788eea12fcbb
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2024-10-24 01:08:03 +0530

    Update Ass_4.py

--- AIML Honor/Ass_4.py
+++ AIML Honor/Ass_4.py
@@ -5,7 +5,7 @@ import seaborn as sns
 from sklearn.decomposition import PCA
 %matplotlib inline
 
-iris-pd.read_csv("/content/iris.csv")
+iris=pd.read_csv("https://media.githubusercontent.com/media/neurospin/pystatsml/refs/heads/master/datasets/iris.csv")
 
 iris.head()
 
@@ -13,10 +13,11 @@ iris.info()
 
 iris.describe()
 
-sns.heatmap(iris.corr())
+sns.heatmap(iris.drop(['species'], axis=1).corr(), annot=True, cmap="coolwarm")
+plt.show()
 
 pca = PCA()
-x_new1 =pca.fit_transform(iris.drop(["variety"], axis =1))
+x_new1 = pca.fit_transform(iris.drop(['species'], axis=1))
 x_new1[:5]
 
 explained_variance=pca.explained_variance_ratio_
@@ -31,17 +32,19 @@ plt.tight_layout()
 plt.show()
 
 
-pcaPCA(n_components=3)
-x_new =pca.fit_transform(iris.drop(['variety'], axis =1))
+pca = PCA(n_components=3)
+x_new = pca.fit_transform(iris.drop(['species'], axis=1))
 x_new[:5]
 
 categ_num={"variety": {"setosa":0,"versicolor":1,"virginica":2}}
 iris1 = iris.replace(categ_num)
 columns= list(iris.columns[:4])
 
-fig, axes = plt.subplots(3,4,figsize=(15,10)) 
-k=0 
-for i in range(axes.shape[0]): 
-    for j in range(axes.shape[1]): 
-        axes[i,j].scatter(x_new[:,i].iris[columns[j]],c="r")
+fig, axes = plt.subplots(3, 4, figsize=(15, 10)) 
+for i in range(3):  # Loop over 3 PCA components
+    for j in range(4):  # Loop over 4 original features
+        axes[i, j].scatter(x_new[:, i], iris[columns[j]], c="r")
+        axes[i, j].set_xlabel(f'PC {i+1}')
+        axes[i, j].set_ylabel(columns[j])
+plt.tight_layout()
 plt.show()
